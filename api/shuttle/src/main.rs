8c35cfd0bd4eed9bc552884c9570374ecb05fb04
Roberto Huertas <roberto.huertas@outlook.com>
2023-07-01 18:00:33 +0200

    add version endpoint

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -8,6 +8,18 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/version")]
+async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
+        .fetch_one(db.get_ref())
+        .await;
+
+    match result {
+        Ok(version) => version,
+        Err(e) => format!("Error: {:?}", e),
+    }
+}
+
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
@@ -17,8 +29,10 @@ async fn actix_web(
         .await
         .map_err(CustomError::new)?;
 
+    let pool = actix_web::web::Data::new(pool);
+
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.app_data(pool).service(hello_world).service(version);
     };
 
     Ok(config.into())
