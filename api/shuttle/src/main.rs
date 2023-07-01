64c93d0cd018ad33b85511247c4d563fdfe07498
Roberto Huertas <roberto.huertas@outlook.com>
2023-07-01 19:51:59 +0200

    added instrumentation

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -10,6 +10,7 @@ async fn hello_world() -> &'static str {
 
 #[get("/version")]
 async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    tracing::info!("Getting version");
     let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
         .fetch_one(db.get_ref())
         .await;
