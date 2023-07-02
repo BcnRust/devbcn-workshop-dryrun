f565da9153f37541dd9f14e69043b96452a0e555
Roberto Huertas <roberto.huertas@outlook.com>
2023-07-02 20:25:18 +0200

    refactor film endpoints to use generics

--- api/lib/src/films.rs
+++ api/lib/src/films.rs
@@ -7,39 +7,40 @@ use uuid::Uuid;
 
 use crate::film_repository::FilmRepository;
 
-type Repository = web::Data<Box<dyn FilmRepository>>;
-
-pub fn service(cfg: &mut ServiceConfig) {
+pub fn service<R: FilmRepository>(cfg: &mut ServiceConfig) {
     cfg.service(
         web::scope("/v1/films")
             // get all films
-            .route("", web::get().to(get_all))
+            .route("", web::get().to(get_all::<R>))
             // get by id
-            .route("/{film_id}", web::get().to(get))
+            .route("/{film_id}", web::get().to(get::<R>))
             // post new film
-            .route("", web::post().to(post))
+            .route("", web::post().to(post::<R>))
             // update film
-            .route("", web::put().to(put))
+            .route("", web::put().to(put::<R>))
             // delete film
-            .route("/{film_id}", web::delete().to(delete)),
+            .route("/{film_id}", web::delete().to(delete::<R>)),
     );
 }
 
-async fn get_all(repo: Repository) -> HttpResponse {
+async fn get_all<R: FilmRepository>(repo: web::Data<R>) -> HttpResponse {
     match repo.get_films().await {
         Ok(films) => HttpResponse::Ok().json(films),
         Err(e) => HttpResponse::NotFound().body(format!("Internal server error: {:?}", e)),
     }
 }
 
-async fn get(film_id: web::Path<Uuid>, repo: Repository) -> HttpResponse {
+async fn get<R: FilmRepository>(film_id: web::Path<Uuid>, repo: web::Data<R>) -> HttpResponse {
     match repo.get_film(&film_id).await {
         Ok(film) => HttpResponse::Ok().json(film),
         Err(_) => HttpResponse::NotFound().body("Not found"),
     }
 }
 
-async fn post(create_film: web::Json<CreateFilm>, repo: Repository) -> HttpResponse {
+async fn post<R: FilmRepository>(
+    create_film: web::Json<CreateFilm>,
+    repo: web::Data<R>,
+) -> HttpResponse {
     match repo.create_film(&create_film).await {
         Ok(film) => HttpResponse::Ok().json(film),
         Err(e) => {
@@ -48,14 +49,14 @@ async fn post(create_film: web::Json<CreateFilm>, repo: Repository) -> HttpRespo
     }
 }
 
-async fn put(film: web::Json<Film>, repo: Repository) -> HttpResponse {
+async fn put<R: FilmRepository>(film: web::Json<Film>, repo: web::Data<R>) -> HttpResponse {
     match repo.update_film(&film).await {
         Ok(film) => HttpResponse::Ok().json(film),
         Err(e) => HttpResponse::NotFound().body(format!("Internal server error: {:?}", e)),
     }
 }
 
-async fn delete(film_id: web::Path<Uuid>, repo: Repository) -> HttpResponse {
+async fn delete<R: FilmRepository>(film_id: web::Path<Uuid>, repo: web::Data<R>) -> HttpResponse {
     match repo.delete_film(&film_id).await {
         Ok(film) => HttpResponse::Ok().json(film),
         Err(e) => {
--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -13,13 +13,12 @@ async fn actix_web(
         .map_err(CustomError::new)?;
 
     let film_repository = api_lib::film_repository::PostgresFilmRepository::new(pool);
-    let film_repository: actix_web::web::Data<Box<dyn api_lib::film_repository::FilmRepository>> =
-        actix_web::web::Data::new(Box::new(film_repository));
+    let film_repository = actix_web::web::Data::new(film_repository);
 
     let config = move |cfg: &mut ServiceConfig| {
         cfg.app_data(film_repository)
             .configure(api_lib::health::service)
-            .configure(api_lib::films::service);
+            .configure(api_lib::films::service::<api_lib::film_repository::PostgresFilmRepository>);
     };
 
     Ok(config.into())
