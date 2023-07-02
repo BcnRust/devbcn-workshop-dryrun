71b5564fa54331d0b439a03c5195c20a7dd2251e
Roberto Huertas <roberto.huertas@outlook.com>
2023-07-02 19:58:23 +0200

    implement film endpoints

--- api/lib/src/films.rs
+++ api/lib/src/films.rs
@@ -2,6 +2,12 @@ use actix_web::{
     web::{self, ServiceConfig},
     HttpResponse,
 };
+use shared::models::{CreateFilm, Film};
+use uuid::Uuid;
+
+use crate::film_repository::FilmRepository;
+
+type Repository = web::Data<Box<dyn FilmRepository>>;
 
 pub fn service(cfg: &mut ServiceConfig) {
     cfg.service(
@@ -19,22 +25,41 @@ pub fn service(cfg: &mut ServiceConfig) {
     );
 }
 
-async fn get_all() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn get_all(repo: Repository) -> HttpResponse {
+    match repo.get_films().await {
+        Ok(films) => HttpResponse::Ok().json(films),
+        Err(e) => HttpResponse::NotFound().body(format!("Internal server error: {:?}", e)),
+    }
 }
 
-async fn get() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn get(film_id: web::Path<Uuid>, repo: Repository) -> HttpResponse {
+    match repo.get_film(&film_id).await {
+        Ok(film) => HttpResponse::Ok().json(film),
+        Err(_) => HttpResponse::NotFound().body("Not found"),
+    }
 }
 
-async fn post() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn post(create_film: web::Json<CreateFilm>, repo: Repository) -> HttpResponse {
+    match repo.create_film(&create_film).await {
+        Ok(film) => HttpResponse::Ok().json(film),
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Internal server error: {:?}", e))
+        }
+    }
 }
 
-async fn put() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn put(film: web::Json<Film>, repo: Repository) -> HttpResponse {
+    match repo.update_film(&film).await {
+        Ok(film) => HttpResponse::Ok().json(film),
+        Err(e) => HttpResponse::NotFound().body(format!("Internal server error: {:?}", e)),
+    }
 }
 
-async fn delete() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn delete(film_id: web::Path<Uuid>, repo: Repository) -> HttpResponse {
+    match repo.delete_film(&film_id).await {
+        Ok(film) => HttpResponse::Ok().json(film),
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Internal server error: {:?}", e))
+        }
+    }
 }
